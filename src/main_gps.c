4b1b78a4e37a52d1e9e484358960da2ae377c086
fabian <fabianpedd@gmail.com>
2020-11-21 00:26:03 +0100

    increase gps message speed to 100hz

--- src/main_gps.c
+++ src/main_gps.c
@@ -19,17 +19,15 @@
 
 atomic_uint last_valid_sample_time = 0;
 
+static can_com_gps_t data;
+static SemaphoreHandle_t gps_data_sem = NULL;
+
 static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
 	gps_t *gps = NULL;
 	switch (event_id) {
 		case GPS_UPDATE:
 
 			gps = (gps_t *)event_data;
-
-			drv_hmc5883_data_t compass_data;
-			memset(&compass_data, 0, sizeof(compass_data));
-			drv_hmc5883_get_data(&compass_data);
-
 			printf("\n--- %d/%d/%d %d:%d:%d ---\n"
 			       "latitude     = %.07fN\n"
 			       "longitude    = %.07fE\n"
@@ -44,10 +42,7 @@ static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_ba
 			       "dop_v        = %f\n"
 			       "variation    = %f\n"
 			       "cog          = %f\n"
-			       "valid        = %d\n"
-			       "compass_x    = %f\n"
-			       "compass_y    = %f\n"
-			       "compass_z    = %f\n",
+			       "valid        = %d\n",
 			       gps->date.year + 2000, gps->date.month, gps->date.day,
 			       gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
 			       gps->latitude,
@@ -63,44 +58,42 @@ static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_ba
 			       gps->dop_v,
 			       gps->variation,
 			       gps->cog,
-			       gps->valid,
-			       compass_data.x,
-			       compass_data.y,
-			       compass_data.z);
+			       gps->valid);
 
-			can_com_gps_t data;
-			memset(&data, 0, sizeof(data));
+			if (xSemaphoreTake(gps_data_sem, 10 / portTICK_PERIOD_MS) == true) {
 
-			data.time = get_time_s();
-			data.second = gps->tim.second;
-			data.minute = gps->tim.minute;
-			data.hour = gps->tim.hour + TIME_ZONE;
-			data.day = gps->date.day;
-			data.month = gps->date.month;
-			data.year = gps->date.year + 2000;
-			data.fix = gps->fix;
-			data.fix_mode = gps->fix_mode;
-			data.sats_in_view = gps->sats_in_view;
-			data.sats_in_use = gps->sats_in_use;
-			data.latitude = gps->latitude;
-			data.longitude = gps->longitude;
-			data.altitude = gps->altitude;
-			data.speed = gps->speed;
-			data.cog = gps->cog;
-			data.dop_h = gps->dop_h;
-			data.dop_p = gps->dop_p;
-			data.dop_v = gps->dop_v;
-			data.variation = gps->variation;
-			data.valid = gps->valid;
-			data.mag_x = compass_data.x;
-			data.mag_y = compass_data.y;
-			data.mag_z = compass_data.z;
+				data.time = get_time_s();
+				data.second = gps->tim.second;
+				data.minute = gps->tim.minute;
+				data.hour = gps->tim.hour + TIME_ZONE;
+				data.day = gps->date.day;
+				data.month = gps->date.month;
+				data.year = gps->date.year + 2000;
+				data.fix = gps->fix;
+				data.fix_mode = gps->fix_mode;
+				data.sats_in_view = gps->sats_in_view;
+				data.sats_in_use = gps->sats_in_use;
+				data.latitude = gps->latitude;
+				data.longitude = gps->longitude;
+				data.altitude = gps->altitude;
+				data.speed = gps->speed;
+				data.cog = gps->cog;
+				data.dop_h = gps->dop_h;
+				data.dop_p = gps->dop_p;
+				data.dop_v = gps->dop_v;
+				data.variation = gps->variation;
+				data.valid = gps->valid;
 
-			can_com_gps_send(data);
+				xSemaphoreGive(gps_data_sem);
+
+			} else {
+				ESP_LOGW(__FILE__, "GPS data sem timeout at mag");
+			}
 
 			if (gps->valid > 0 && gps->fix > 0 && gps->fix_mode > 1) {
 				last_valid_sample_time = get_time_ms();
 			}
+
 			break;
 
 		case GPS_UNKNOWN:
@@ -117,6 +110,13 @@ void main_gps() {
 
 	delay_ms(1000);
 
+	memset(&data, 0, sizeof(data));
+
+	gps_data_sem = xSemaphoreCreateMutex();
+	if (gps_data_sem == NULL) {
+		ESP_LOGE(__FILE__, "Cant create GPS data mutex");
+	}
+
 	/* NMEA parser configuration */
 	nmea_parser_config_t config = NMEA_PARSER_CONFIG_DEFAULT();
 
@@ -132,13 +132,37 @@ void main_gps() {
 
 	while (1) {
 
+		drv_hmc5883_data_t compass_data;
+		memset(&compass_data, 0, sizeof(compass_data));
+		drv_hmc5883_get_data(&compass_data);
+
+		printf("compass_x    = %f\n"
+		       "compass_y    = %f\n"
+		       "compass_z    = %f\n",
+		       compass_data.x,
+		       compass_data.y,
+		       compass_data.z);
+
+		if (xSemaphoreTake(gps_data_sem, 10 / portTICK_PERIOD_MS) == true) {
+			data.mag_x = compass_data.x;
+			data.mag_y = compass_data.y;
+			data.mag_z = compass_data.z;
+
+			can_com_gps_send(data);
+
+			xSemaphoreGive(gps_data_sem);
+
+		} else {
+			ESP_LOGW(__FILE__, "GPS data sem timeout at mag");
+		}
+
 		if (get_time_ms() < last_valid_sample_time + 2000) {
 			drv_led_set(LED_ON_ALIVE);
 		} else {
 			drv_led_set(LED_FAST);
 		}
 
-		delay_until_ms(&last_wake_time, 100);
+		delay_until_ms(&last_wake_time, 10);
 	}
 
 	// If we get here, something went badly wrong. Reset the system

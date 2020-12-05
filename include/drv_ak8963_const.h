caa09a5c20a329d54b69686f24e59058242ff471
fabian <fabianpedd@gmail.com>
2020-12-05 02:16:48 +0100

    remove stray characters

--- include/drv_ak8963_const.h
+++ include/drv_ak8963_const.h
@@ -1,6 +1,6 @@
 #ifndef DRV_AK8963_CONST_H
 #define DRV_AK8963_CONST_H
-​
+
 // == SOFT IRON ================================================================
 #define MAG_SI_11      86.8227209
 #define MAG_SI_12      -0.2403698
@@ -11,17 +11,17 @@
 #define MAG_SI_31       0.8596319
 #define MAG_SI_32      -0.4638596
 #define MAG_SI_33      88.4975581
-​
+
 // == HARD IRON ================================================================
 // Mcorr = SI*(M-HI)
 #define MAG_HI_1       15.8836179
 #define MAG_HI_2       51.5660664
 #define MAG_HI_3      -44.1729453
-​
+
 // == CURRENT SCALING ==========================================================
 // MCScorr = Mcorr - CS*curr
 #define MAG_CS_1        0.2485534
 #define MAG_CS_2        0.3516560
 #define MAG_CS_3       -0.2448827
-​
+
 #endif // DRV_AK8963_CONST_H
